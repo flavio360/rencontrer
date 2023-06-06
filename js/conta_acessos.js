8ceeb8e0b974a1e83037ddc6858cd5eb735a23be
Your Nameflavio <flaviomachadolopes360@gmail.com>
2023-06-06 17:52:08 -0300

    v3

---
+++ js/conta_acessos.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// Ler o valor do arquivo
+fs.readFile('arquivo.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Converter o valor lido para número e incrementá-lo em 1
+  let valor = parseInt(data);
+  valor += 1;
+
+  // Salvar o novo valor no arquivo
+  fs.writeFile('arquivo.txt', valor.toString(), 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    //console.log('Novo valor salvo com sucesso!');
+  });
+});
