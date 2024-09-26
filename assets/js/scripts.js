4a2cc39a906700f14a30bdcea367dda5252672b4
flavio.lopes <flavio.lopes@lupeon.com.br>
2024-09-26 12:45:53 -0300

    att

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,3 +23,30 @@ document.addEventListener("DOMContentLoaded", function() {
         contentSection.innerHTML = '<h2>Conheça a Rencontrer</h2><p>Descrição sobre a Rencontrer será carregada aqui.</p>';
     }
 });
+
+
+function initCarousel(containerSelector, imageDir, imageList, interval = 4000) {
+    const container = document.querySelector(containerSelector);
+    const carousel = container.querySelector('.carousel');
+    let currentIndex = 0;
+
+    // Função para carregar as imagens
+    function loadImages() {
+        imageList.forEach(image => {
+            const imgElement = document.createElement('img');
+            imgElement.src = `${imageDir}/${image}`;
+            carousel.appendChild(imgElement);
+        });
+    }
+
+    // Função para trocar as imagens
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % imageList.length;
+        const offset = -currentIndex * 100;
+        carousel.style.transform = `translateX(${offset}%)`;
+    }
+
+    loadImages(); // Carregar as imagens ao iniciar
+    setInterval(nextImage, interval); // Muda a imagem a cada intervalo definido (padrão: 4 segundos)
+}
+
